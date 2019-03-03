2665092e11f4c88daa826afbab1476764adf00ea
Chris <48174822+ChrisGME4@users.noreply.github.com>
2019-03-03 09:39:07 +0000

    Initial commit

---
+++ EF6-Code-First/EF6-Code-First/EF6CodeFirstDemo/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF6CodeFirstDemo
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        //fully defined relationship
+        public int? GradeId { get; set; }
+
+        public virtual Grade Grade { get; set; }
+
+        public virtual StudentAddress Address { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
